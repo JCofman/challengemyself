f8d234937cadd209ac14d08c4fb26e97c6856ca6
Ixl123 <cofman.jacob@gmx.de>
2019-10-12 23:23:14 +0200

    try first working build

--- src/components/app.js
+++ src/components/app.js
@@ -6,30 +6,26 @@ import Header from './header';
 
 // Code-splitting is automated for routes
 import Home from '../routes/home';
-import Profile from '../routes/profile';
 
 const App = () => {
+  /** Gets fired when the route changes.
+   *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
+   *	@param {string} event.url	The newly routed URL
+   */
+  const handleRoute = e => {
+    this.currentUrl = e.url;
+  };
 
-	/** Gets fired when the route changes.
-	 *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
-	 *	@param {string} event.url	The newly routed URL
-	 */
-	const handleRoute = e => {
-		this.currentUrl = e.url;
-	};
-
-	return (
-		<div id="app">
-			<ProvideAuth>
-				<Header />
-				<Router onChange={handleRoute}>
-					<Home path="/" />
-					<Profile path="/profile/" user="me" />
-					<Profile path="/profile/:user" />
-				</Router>
-			</ProvideAuth>
-		</div>
-	);
+  return (
+    <div id="app">
+      <ProvideAuth>
+        <Header />
+        <Router onChange={handleRoute}>
+          <Home path="/" />
+        </Router>
+      </ProvideAuth>
+    </div>
+  );
 };
 
 export default App;
--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,39 +1,40 @@
 // Hook (use-auth.js)
-import { h, createContext } from 'preact'
-import { useState, useEffect, useContext } from 'preact/hooks'
-import * as firebase from 'firebase/app'
-import 'firebase/auth'
+import { h, createContext } from 'preact';
+import { useState, useEffect, useContext } from 'preact/hooks';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
 
 // Add your Firebase credentials
-firebase.initializeApp({
-  apiKey: 'AIzaSyBhUdUbAfkxCcNSqP14LE4uwQ2Bo9TNqRk',
-  authDomain: 'challengemyself-f65a8.firebaseapp.com',
-  databaseURL: 'https://challengemyself-f65a8.firebaseio.com',
-  projectId: 'challengemyself-f65a8',
-  storageBucket: 'challengemyself-f65a8.appspot.com',
-  messagingSenderId: '905777837799',
-  appId: '1:905777837799:web:8af8c94b236c7974821442',
-  measurementId: 'G-GWQ066XZVK',
-})
 
-const googleProvider = new firebase.auth.GoogleAuthProvider()
-
-const authContext = createContext()
+if (typeof window !== 'undefined') {
+  firebase.initializeApp({
+    apiKey: 'AIzaSyBhUdUbAfkxCcNSqP14LE4uwQ2Bo9TNqRk',
+    authDomain: 'challengemyself-f65a8.firebaseapp.com',
+    databaseURL: 'https://challengemyself-f65a8.firebaseio.com',
+    projectId: 'challengemyself-f65a8',
+    storageBucket: 'challengemyself-f65a8.appspot.com',
+    messagingSenderId: '905777837799',
+    appId: '1:905777837799:web:8af8c94b236c7974821442',
+    measurementId: 'G-GWQ066XZVK',
+  });
+  const googleProvider = new firebase.auth.GoogleAuthProvider();
+  const authContext = createContext();
+}
 
 // Provider component that wraps your app and makes auth object ...
 // ... available to any child component that calls useAuth().
 export function ProvideAuth({ children }) {
-  const auth = useProvideAuth()
-  return <authContext.Provider value={auth}>{children}</authContext.Provider>
+  const auth = useProvideAuth();
+  return <authContext.Provider value={auth}>{children}</authContext.Provider>;
 }
 
 // Hook for child components to get the auth object ...
 // ... and re-render when it changes.
-export const useAuth = () => useContext(authContext)
+export const useAuth = () => useContext(authContext);
 
 // Provider hook that creates auth object and handles state
 function useProvideAuth() {
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState(null);
 
   // Wrap any Firebase methods we want to use making sure ...
   // ... to save the user to state.
@@ -42,47 +43,47 @@ function useProvideAuth() {
       .auth()
       .signInWithEmailAndPassword(email, password)
       .then(response => {
-        setUser(response.user)
-        return response.user
-      })
+        setUser(response.user);
+        return response.user;
+      });
 
   const signInWithGoogle = () => {
     firebase
       .auth()
       .signInWithPopup(googleProvider)
       .then(response => {
-        setUser(response.user)
-        return response.user
-      })
-  }
+        setUser(response.user);
+        return response.user;
+      });
+  };
   const signup = (email, password) =>
     firebase
       .auth()
       .createUserWithEmailAndPassword(email, password)
       .then(response => {
-        setUser(response.user)
-        return response.user
-      })
+        setUser(response.user);
+        return response.user;
+      });
 
   const signout = () =>
     firebase
       .auth()
       .signOut()
       .then(() => {
-        setUser(false)
-      })
+        setUser(false);
+      });
 
   const sendPasswordResetEmail = email =>
     firebase
       .auth()
       .sendPasswordResetEmail(email)
-      .then(() => true)
+      .then(() => true);
 
   const confirmPasswordReset = (code, password) =>
     firebase
       .auth()
       .confirmPasswordReset(code, password)
-      .then(() => true)
+      .then(() => true);
 
   // Subscribe to user on mount
   // Because this sets state in the callback it will cause any ...
@@ -91,15 +92,15 @@ function useProvideAuth() {
   useEffect(() => {
     const unsubscribe = firebase.auth().onAuthStateChanged(user => {
       if (user) {
-        setUser(user)
+        setUser(user);
       } else {
-        setUser(false)
+        setUser(false);
       }
-    })
+    });
 
     // Cleanup subscription on unmount
-    return () => unsubscribe()
-  }, [])
+    return () => unsubscribe();
+  }, []);
 
   // Return the user object and auth methods
   return {
@@ -110,5 +111,5 @@ function useProvideAuth() {
     sendPasswordResetEmail,
     confirmPasswordReset,
     signInWithGoogle,
-  }
+  };
 }
