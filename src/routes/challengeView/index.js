bcaf2fb65f419e4cd4a49423ae1e362eca5696d0
Daniel Eckelt <deckelt@inovex.de>
2019-10-13 00:13:51 +0200

    Set up create challenge page

--- src/routes/challengeView/index.js
+++ src/routes/challengeView/index.js
@@ -1,40 +1,61 @@
-import React, { useState, useEffect } from 'react';
-import style from './challengeView.css';
-import fallbackImg from '../../assets/fallback-image.jpeg';
+import { h } from 'preact'
+import { useState, useEffect } from 'preact/hooks'
+import style from './challengeView.css'
+import fallbackImg from '../../assets/fallback-image.jpeg'
 
 const ChallengeView = () => {
-  const [imgUrl, setImgUrl] = useState('');
+  const [imgUrl, setImgUrl] = useState(fallbackImg);
+
+  const clientId= '9c2b0b52027502b5e790640d080938e6efe192ddef317faaec51b8d8bbb15b7e';
+  const imagesQuery = window.location.pathname.slice(1) || 'trex'; // because it starts with /
+  const unsplashUrl = `https://api.unsplash.com/photos/random?client_id=${clientId}&query=${imagesQuery}`;
 
   useEffect(() => {
     // TODO: fetch image from firebase of unsplash
-    fetch('https://images.unsplash.com/photo-1500933964569-522caa01ca2e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2252&q=80')
-      .then((response) => {
-        // no response for fetch request, use fallback image!
-        if (!response) {
-          setImgUrl(fallbackImg);
-        }
-        console.log(response);
-        setImgUrl(response.url);
-        return response;
-      }, (e) => {
-        console.error(e);
-      });
-  }, []);
+    fetch(unsplashUrl)
+        .then(response => response.json())
+      	.then(json => json.urls.regular)
+      	.then(picUrl => {
+          console.log('url', picUrl);
+      		setImgUrl(picUrl);
+      	});
+      },
+      e => {
+        console.error(e)
+      },
+      [unsplashUrl]
+    );
 
   return (
     <div class={style.root}>
-      <div class={style.heroImage} style={{ 'background-image': `url('${imgUrl}')` }} />
-      <div style={{ display: 'flex', 'justify-content': 'space-between', 'align-items': 'center' }}>
+      <div
+        class={style.heroImage}
+        style={{ 'background-image': `url('${imgUrl}')` }}
+      />
+      <div
+        style={{
+          display: 'flex',
+          'justify-content': 'space-between',
+          'align-items': 'center',
+        }}
+      >
         <div class={style.backToOverview}>Back to Overview</div>
       </div>
-      <div style={{ display: 'flex', 'justify-content': 'center', 'align-items': 'center', width: '100%', height: '100%' }}>
-        <div class={style.daysToGo}></div>
+      <div
+        style={{
+          display: 'flex',
+          'flex-direction': 'column',
+          'justify-content': 'center',
+          'align-items': 'center',
+          width: '100%',
+          height: '100%',
+        }}
+      >
+        <div class={style.title}>Work hard</div>
+        <div class={style.daysToGo}>78</div>
       </div>
     </div>
-  );
+  )
 }
 
-
-export default ChallengeView;
-
-
+export default ChallengeView
