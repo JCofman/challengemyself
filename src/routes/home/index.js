9d9946fe0e54b2a729038fde10bae3202be12a64
Ixl123 <cofman.jacob@gmx.de>
2019-10-12 22:54:51 +0200

    add logout button

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,20 +1,23 @@
-import { h } from 'preact';
-import style from './style';
-import { useAuth } from '../../hooks/useAuth';
+import { h } from 'preact'
+import style from './style'
+import { useAuth } from '../../hooks/useAuth'
 
 const Home = () => {
-	const auth = useAuth();
-	return (
-		<div class={style.home}>
-			<h1>Home</h1>
-			<p>This is the Home component.</p>
-			{auth.user ? (
-				<div>I AM SIGNED INED</div>
-			) : (
-				<button onClick={() => auth.signInWithGoogle()}>Sign in</button>
-			)}
-		</div>
-	);
-};
+  const auth = useAuth()
+  return (
+    <div class={style.home}>
+      <h1>Home</h1>
+      <p>This is the Home component.</p>
+      {auth.user ? (
+        <div>
+          <div>I AM SIGNED IN</div>
+          <button onClick={() => auth.signout()}>logout</button>
+        </div>
+      ) : (
+        <button onClick={() => auth.signInWithGoogle()}>Sign in</button>
+      )}
+    </div>
+  )
+}
 
-export default Home;
+export default Home
