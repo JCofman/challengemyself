1dc858408ab6cbdb297f8f7b6fee47ed7305405f
Daniel Eckelt <deckelt@inovex.de>
2019-10-13 10:29:54 +0200

    Make create challenge view more beatiful

--- src/routes/createChallenge/index.js
+++ src/routes/createChallenge/index.js
@@ -1,35 +1,57 @@
-import { h } from 'preact'
-import { useState } from 'preact/hooks'
-import style from './createChallenge.css'
+import { h } from 'preact';
+import { useState } from 'preact/hooks';
+import style from './createChallenge.css';
 
 const CreateChallenge = () => {
   const [name, setName] = useState('');
   const [from, setFrom] = useState(new Date());
-  const [duration, setDuration] = useState(0);
+  const [duration, setDuration] = useState(100);
 
   const submitChallenge = () => {
     console.log('create Challenge');
     if (window) window.location.assign('trex');
-  }
+  };
 
   return (
     <div class={style.root}>
-     <form onSubmit={submitChallenge}>
-       <div style={{ display: 'flex', 'flex-direction': 'column', 'align-items': 'center' }}>
-         <label for="name">Your next challenge:</label>
-         <input id="name" type="text" value={name} aria-label="challenge-name" />
+      <form onSubmit={submitChallenge}>
+        <div
+          style={{
+            display: 'flex',
+            'flex-direction': 'column',
+            'align-items': 'center',
+          }}
+        >
+          <label for="name">Your next challenge</label>
+          <input
+            id="name"
+            type="text"
+            value={name}
+            required
+            aria-label="challenge-name"
+            class={style.title}
+          />
 
-         <label for="from">Start on:</label>
-         <input id="from" type="date" value={from} aria-label="challenge-start-on-value" />
+          <label for="duration">Duration: {duration}</label>
+          <input
+            id="duration"
+            type="number"
+            min="5"
+            max="365"
+            value={duration}
+            required
+            onchange={e => setDuration(e.target.value)}
+            aria-label="challenge-duration"
+            class={style.duration}
+          />
 
-         <label for="duration">Duration: {duration}</label>
-         <input id="duration" type="range" min="0" max="100" value={duration} onchange={(e) => setDuration(e.target.value)} aria-label="challenge-duration" />
-
-         <button class="create-button">Create Challenge</button>
-       </div>
-     </form>
+          <button type="submit" class={style.createButton}>
+            Create Challenge
+          </button>
+        </div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default CreateChallenge
+export default CreateChallenge;
