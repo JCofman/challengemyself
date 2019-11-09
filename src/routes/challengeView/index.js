a9c6c9e3b24de8e2bbe61fb617fa1ec2e205b071
Daniel Eckelt <deckelt@inovex.de>
2019-11-09 17:35:48 +0100

    Add absolutely insance titl effect! 🤘

--- src/routes/challengeView/index.js
+++ src/routes/challengeView/index.js
@@ -1,17 +1,23 @@
 import { h } from 'preact';
 import { useState, useEffect } from 'preact/hooks';
+import tilt from 'vanilla-tilt';
+
 import { useDatabaseEntry } from '../../hooks/useDatabaseEntry';
 import style from './challengeView.css';
 import fallbackImgUrl from '../../assets/fallback-image.jpeg';
 import { calcDaysToGo } from '../../utils';
 
+
 const ChallengeView = (prop) => {
   const [imgUrl, setImgUrl] = useState('');
+
   const clientId = '9c2b0b52027502b5e790640d080938e6efe192ddef317faaec51b8d8bbb15b7e';
   const challengeIdUrl = window.location.pathname.slice(1); // because it starts with '/'
   const [duration, name, createdDate] = useDatabaseEntry(challengeIdUrl);
   const unsplashUrl = `https://api.unsplash.com/photos/random?client_id=${clientId}&query=${name}`;
 
+  console.log(tilt);
+  tilt.init(document.querySelector('[data-tilt]'));
   useEffect(() => {
     // fetch bg image
     fetch(unsplashUrl)
@@ -29,9 +35,11 @@ const ChallengeView = (prop) => {
         style={{ 'background-image': `url('${imgUrl || fallbackImgUrl}')` }}
       />
       <div class={style.challenge}>
-        <div class={style.challenge__titleAndDays}>
-          <div class={style.challenge__title}>{name ? name.toUpperCase() : '...'}</div>
-          <div class={style.challenge__daysToGo}>{duration && createdDate ? calcDaysToGo(duration, createdDate) : 'XX'}</div>
+        <div data-tilt class={style.challenge__titleAndDays_Wrapper}>
+          <div class={style.challenge__titleAndDays}>
+            <div class={style.challenge__title}>{name ? name.toUpperCase() : '...'}</div>
+            <div class={style.challenge__daysToGo}>{duration && createdDate ? calcDaysToGo(duration, createdDate) : 'XX'}</div>
+          </div>
         </div>
         <div class={style.challenge__backToOverview}>Back to Overview</div>
       </div>
