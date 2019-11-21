c64e59013fdc4dbd95553c20ce9bf850b7b2f395
Daniel Eckelt <deckelt@inovex.de>
2019-11-21 08:03:50 +0100

    Add nice hover effect with filling bg

--- src/routes/challengesOverview/index.js
+++ src/routes/challengesOverview/index.js
@@ -2,7 +2,7 @@ import { h } from 'preact';
 import { Link } from 'preact-router/match';
 import { useDatabaseEntry } from '../../hooks/useDatabaseEntry';
 import style from './challengesOverview.css';
-import { calcDaysToGo } from '../../utils';
+import { calcDaysToGo, formatDate } from '../../utils';
 
 
 const ChallengesOverview = () => {
@@ -21,22 +21,17 @@ const ChallengesOverview = () => {
               Object.entries(allChallenges)
                 .sort(([key1, c1], [key2, c2]) => c2.startDate - c1.startDate)
                 .map(([key, challenge]) => (
-                  <Link href={`/${userId}/challenges/${key}`} class={style.link}>
-                    <div
-                      key={challenge.name + challenge.startDate}
-                      class={style.item}
-                    >
-                        <div class={style.name}>{challenge.name}</div>
-                        <div class={style.time}>
-                          <div class={style.daysToGo}>
-                            {calcDaysToGo(challenge.duration, challenge.startDate)}
-                          </div>
-                          /
-                          <div class={style.duration}>{challenge.duration}</div>
-                        </div>
-                        <div class={style.start}>
-                          Started on: {new Date(challenge.startDate).toDateString()}
-                        </div>
+                  <Link href={`/${userId}/challenges/${key}`} class={style.item} key={challenge.name + challenge.startDate}>
+                    <div class={style.name}>{challenge.name}</div>
+                    <div class={style.time}>
+                      <div class={style.daysToGo}>
+                        {calcDaysToGo(challenge.duration, challenge.startDate)}
+                      </div>
+                      /
+                      <div class={style.duration}>{challenge.duration}</div>
+                    </div>
+                    <div class={style.start}>
+                      Started on: {formatDate(new Date(challenge.startDate))}
                     </div>
                   </Link>
                 ))
